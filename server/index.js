e427afe5267e7eb90f9d8311cb6c818c446cf92b
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-29 00:03:07 +0530

    implemented zod and completed with the

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const mainRouter = require('./router/mainrouter')
+const cors = require('cors')
+app.use(express.json())
 app.get('/', (req, res) => {
     res.json({ message: "hello" })
 })
 
+app.use(cors())
 app.use('/v1', mainRouter)
 
 app.listen(PORT, () => {
--- server/router/user.js
+++ server/router/user.js
@@ -1,24 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const { PrismaClient } = require("@prisma/client")
-
+const zod = require('zod')
+const bcrypt = require('bcrypt')
 const prisma = new PrismaClient()
 // Sign In Controller
+
+const signinSchema = zod.object({
+    email: zod.string().email(),
+    password: zod.string(),
+    name: zod.string(),
+    Department: zod.string()
+})
 router.post('/signin', async (req, res) => {
 
-    const body = await req.body
-    
+    const { success } = await signinSchema.safeParse(req.body)
+    if (!success) {
+        return res.status(401).json({ message: "Incorrect input" })
+    }
+
+    const saltRound = 10
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRound)
+
     const response = await prisma.user.create({
         data: {
-            email: "pandeym89s1@gmail.com",
-            password: "12345678",
-            name: "mridul",
-            Department: "admin"
+            email: req.body.email,
+            password: hashedPassword,
+            name: req.body.name,
+            Department: req.body.Department
         }
     })
 
-    res.json({ message: "Done" })
-}
-)
+    res.json(response   )
+})
 
 module.exports = router
