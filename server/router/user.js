8bb55373dcf081b3438a0353f3eee8a478709fbe
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-29 10:55:17 +0530

    Completed signup route

--- server/router/user.js
+++ server/router/user.js
@@ -4,6 +4,8 @@ const { PrismaClient } = require("@prisma/client")
 const zod = require('zod')
 const bcrypt = require('bcrypt')
 const prisma = new PrismaClient()
+const jwt = require('jsonwebtoken')
+const JWTSECRET = "HelloMridul"
 // Sign In Controller
 
 const signinSchema = zod.object({
@@ -22,6 +24,17 @@ router.post('/signin', async (req, res) => {
     const saltRound = 10
     const hashedPassword = await bcrypt.hash(req.body.password, saltRound)
 
+    // Exisiting user 
+    const ExisitingUser = await prisma.user.findFirst({
+        where: {
+            email: req.body.email
+        }
+    })
+
+    if (ExisitingUser) {
+        return res.status(401).json({ message: "User Already Existed" })
+    }
+
     const response = await prisma.user.create({
         data: {
             email: req.body.email,
@@ -31,7 +44,10 @@ router.post('/signin', async (req, res) => {
         }
     })
 
-    res.json(response   )
+    const token = jwt.sign({ id: response.id }, JWTSECRET)
+    console.log(token)
+    res.status(201).json({ token: token })
+
 })
 
 module.exports = router
